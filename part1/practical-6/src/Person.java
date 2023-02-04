7dc2b6aa4ec78c01ca299098f3945a7db821cd69
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-02-04 19:00:44 +0000

    Practical 6 Completed comments

--- part1/practical-6/src/Person.java
+++ part1/practical-6/src/Person.java
@@ -1,19 +1,102 @@
 public class Person{
+    private String name;
+    private double height;
+    private double weight;
 
-    /*Complete*/
+    public Person(String name, double height, double weight){
+        this.name = name;
+        postitiveCheck(height, " Height");
+        this.height = height;
+        postitiveCheck(weight, " Weight");
+        this.weight = weight;
 
 
-    public String format(){
-        return String.format( "Name:\t%s", name );
     }
+    private void postitiveCheck(double x, String name){
+        if (x<0){
+            throw new IllegalArgumentException(name + "Must be a positive decimal number");
+        }
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHeight() {
+        postitiveCheck(height, " Height:");
+        return height;
+    }
+
+    public void setHeight(double height) {
+        postitiveCheck(height, "Weight:");
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double BMI(){
+        return getWeight() / (getHeight() * getHeight());
+    }
+
+    public void setImperial(double inches, double stone){
+        setWeight(stone * 6.35);
+        setHeight(inches * 2.54 /100);
+
+    }
+
+    public String format() {
+        return String.format("Name:\t%s\n" +
+                        "Height (m):\t%s\n" +
+                        "Weight (kg):\t%s\n" +
+                        "BMI:\t%s\n",
+                getName(), getHeight(),
+                getWeight(), bmiStatus());
+    }
+
+
 
     public void display(){
-        System.out.println( format() );
+
+        System.out.println(format());
     }
+
+    public String bmiStatus(){
+        double bmi = BMI();
+
+        if (bmi < 18.5){
+            return "Underweight";
+        } if (bmi < 25) {
+            return "Normal";
+        } if (bmi < 30) {
+            return "Overweight";
+        } if (bmi < 35) {
+            return "Obese";
+        }
+        return "Extremely Obese";
+    }
+
 
 
     public static void main(String[] args){
-        Person p = new Person("Harry Potter");
+        Person p = new Person("Harry Potter", 0, 0);
+        p.setImperial(72, 11);
+        p.display();
+
+
+
+
 
         // call the display function here
 
