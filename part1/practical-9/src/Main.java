53c10c7abeb11ef5ed70006cae5fc60227e2dfb7
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-02-05 11:00:54 +0000

    Added scanner

--- part1/practical-9/src/Main.java
+++ part1/practical-9/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     private Car c;
     private Garage g;
@@ -8,19 +10,26 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Car c1 = new Car("Audi", "A4", 20);
-        Car c2 = new Car("BMW", "420i", 20);
-        Garage g = new Garage("Aarons Autos", "36 Dodgysite street", "DL5 6TG");
+        Garage g = new Garage("Aaron's Autos", "36 Dodgysite street", "DL5 6TG");
 
         g.addCar(c1);
         System.out.println(g.getCarsInGarage());
 
-        System.out.println(g.checkCar(c1));
-        System.out.println(g.checkCar(c2));
+        System.out.println("Enter the car details you want to add: ");
+        System.out.print("Brand: ");
+        String brand = input.nextLine();
+        System.out.print("Model: ");
+        String model = input.nextLine();
+        System.out.print("Age: ");
+        int age = input.nextInt();
+        Car userCar = new Car(brand, model, age);
+        g.addCar(userCar);
+        System.out.println("\nCars in the garage after adding user car:");
+        System.out.println(g.getCarsInGarage());
 
-        System.out.println(g.showCarsGivenAge(19));
-
-
+        input.close();
 
     }
 }
